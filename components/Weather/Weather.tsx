4d4c336627adbbda76295802e0294d628ea4cb3e
Daniel <danirvicente@gmail.com>
2022-08-21 17:46:04 +0200

    component WeatherData

--- components/Weather/Weather.tsx
+++ components/Weather/Weather.tsx
@@ -1,9 +1,7 @@
-import { Button, DatePicker, Space } from "antd"
-import { Typography } from "antd"
 import { Header } from "../Header"
 import useWeather from "../../hooks/useWeather"
 import WeatherForm from "./WeatherForm"
-const { Title } = Typography
+import WeatherData from "./WeatherData"
 
 export default function Weather() {
     const { weather, setCity, callApi, city } = useWeather()
@@ -12,29 +10,12 @@ export default function Weather() {
     return (
         <>
             <Header>{"What's the Weather"}</Header>
-            <Title level={2} style={{ color: "white" }}>
-                Select a date to see the weather
-            </Title>
-            <Space direction="vertical" size="middle" style={{ display: "flex" }}>
-                <Space>
-                    <DatePicker />
-                    <Button type="primary" style={{ marginLeft: 8 }}>
-                        Date
-                    </Button>
-                </Space>
-                <WeatherForm setCity={setCity} callApi={callApi} />
-            </Space>
-            {weather ? (
-                <div>
-                    <p className="city-title" style={{ color: "#7928ca" }}>
-                        {city.toUpperCase()}
-                    </p>
-                    <p className="temperature">{temperature} °C</p>
-                    <p className="wind">Wind at this moment is {wind} km/h</p>
-                    <p className="resume">{resume}</p>
-                    <p className="description">{description}</p>
-                </div>
-            ) : null}
+            <WeatherForm setCity={setCity} callApi={callApi} />
+            {WeatherData ? <WeatherData city={city} temperature={temperature} wind={wind} resume={resume} description={description} /> : null}
         </>
     )
+}
+
+{
+    /* Removed component DatePicker before WeatherForm */
 }
